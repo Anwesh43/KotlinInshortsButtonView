ea59c7259c51f646f0b610c943ee7cbebc34ce2c
Anwesh43 <anweshthecool0@gmail.com>
2018-05-28 03:06:02 +0530

    adding fullscreen support

--- app/src/main/java/com/example/anweshmishra/kotlininshortslogoview/MainActivity.kt
+++ app/src/main/java/com/example/anweshmishra/kotlininshortslogoview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.anweshmishra.kotlininshortslogoview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.example.inshortslogoview.InshortsLogoView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         InshortsLogoView.create(this)
+        fullScreen()
     }
+}
+
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
