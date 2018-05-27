b809ce1505bd3baf3708535136a9533ddd127035
Anwesh43 <anweshthecool0@gmail.com>
2018-05-28 03:04:31 +0530

    adding view in activity

--- app/src/main/java/com/example/anweshmishra/kotlininshortslogoview/MainActivity.kt
+++ app/src/main/java/com/example/anweshmishra/kotlininshortslogoview/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.anweshmishra.kotlininshortslogoview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.inshortslogoview.InshortsLogoView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        InshortsLogoView.create(this)
     }
 }
