5ce5b8d5de5d1a8981bdf3348b4e8be9ae3a3650
Anwesh43 <anweshthecool0@gmail.com>
2018-05-28 01:19:44 +0530

    adding inshortslogoview module

---
+++ inshortslogoview/src/main/java/com/example/inshortslogoview/InshortsLogoView.kt
@@ -0,0 +1,11 @@
+package com.example.inshortslogoview
+
+/**
+ * Created by anweshmishra on 28/05/18.
+ */
+
+import android.content.Context
+import android.graphics.*
+import android.view.View
+import android.view.MotionEvent
+
